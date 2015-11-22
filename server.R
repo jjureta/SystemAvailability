de46aa3131de87f97b0b21259e32dc8801538d15
jjureta <josip.jureta@gmail.com>
2015-11-22 07:24:34 -0500

    make basic layout

--- server.R
+++ server.R
@@ -6,18 +6,35 @@
 #
 
 library(shiny)
+library(ggplot2)
+require(googleVis)
+
+dat <- data.frame(Room=c("Room 1","Room 2","Room 3"),
+                  Language=c("English", "German", "French"),
+                  start=as.POSIXct(c("2014-03-14 14:00", 
+                                     "2014-03-14 15:00",
+                                     "2014-03-14 14:30")),
+                  end=as.POSIXct(c("2014-03-14 15:00", 
+                                   "2014-03-14 16:00",
+                                   "2014-03-14 15:30")))
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  # a large table, reative to input$show_vars
+  output$plot <- renderGvis({
+    gvisTimeline(data = dat, 
+                 rowlabel = "Room", barlabel = "Language", 
+                 start = "start", end = "end")
   })
-
+  
+  # sorted columns are colored now because CSS are attached to them
+  output$summary <- renderDataTable({
+    mtcars
+  }, options = list(orderClasses = TRUE))
+  
+  # customize the length drop-down menu; display 5 rows per page by default
+  output$table <- renderDataTable({
+    diamonds
+  }, options = list(lengthMenu = c(5, 30, 50), pageLength = 15))
+  
 })
